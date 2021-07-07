8b27e129ff61d0dc6db80acf4dbc06f398517678
lNKU <wcarpen4@gmail.com>
2021-07-06 22:36:03 -0400

    updated js script

--- scripts/script.js
+++ scripts/script.js
@@ -15,36 +15,48 @@ function userPlay() {
 	return userChoice;
 }
 
-console.log(userPlay());
+// console.log(userPlay());
 
 function computerPlay() {
-	const a = Math.floor(Math.random() * 3) + 1;
-	let b = a.toString();
+	let a = Math.floor(Math.random() * 3) + 1;
+
 	if (a === 1) {
-		return (b = 'rock');
+		return (a = 'rock');
 	} else if (a === 2) {
-		return (b = 'paper');
-	} else {
-		return (b = 'scissors');
+		return (a = 'paper');
+	} else if (a === 3) {
+		return (a = 'scissors');
 	}
 }
 
-console.log(computerPlay());
+// console.log(computerPlay());
 
 function playRound(userSelection, computerSelection) {
-	const userSelection = userPlay();
-	const computerSelection = computerPlay();
+	//Rock outcomes
+	if (computerSelection == 'rock' && userSelection == 'paper') {
+		return 'Player wins!';
+	} else if (userSelection == 'rock' && computerSelection == 'paper') {
+		return 'Computer wins!';
 
-	//tie game condition result
-	if (computerSelection === userSelection) {
-		return 'Tie game!';
-
-		//Rock outcomes
-	} else if (computerSelection === 'rock' && userSelection === 'paper') {
-		return 'User wins!';
 		//Paper outcomes
-	} else if (computerSelection === 'paper' && userSelection === 'scissors') {
+	} else if (computerSelection == 'paper' && userSelection == 'scissors') {
+		return 'Player wins!';
+	} else if (userSelection == 'paper' && computerSelection == 'scissors') {
+		return 'Computer wins!';
+
 		//Scissors outcomes
-	} else if (computerSelection === 'scissors' && userSelection === 'paper') {
+	} else if (computerSelection == 'scissors' && userSelection == 'rock') {
+		return 'Player wins!';
+	} else if (userSelection == 'scissors' && computerSelection == 'rock') {
+		return 'Computer wins!';
+	} else if (computerSelection == userSelection) {
+		return 'Tie game!';
 	}
 }
+
+const userSelection = userPlay();
+const computerSelection = computerPlay();
+
+console.log(`Player chose ${userSelection}!`);
+console.log(`Computer chose ${computerSelection}!`);
+console.log(playRound(userSelection, computerSelection));
